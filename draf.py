f2d6d9aad27301b717af65d6df87c48cbb280804
Luke Nguyen <luke.nguyen@cgsystemsinc.com>
2019-11-25 15:33:04 -0800

    reading excel and iterrow the file

--- draf.py
+++ draf.py
@@ -1,11 +1,18 @@
+import xlrd
 import pandas as pd
 from pandas import ExcelWriter
 from pandas import ExcelFile
 
 # load the sheet into the Pandas Dataframe structure
-df = pd.read_excel('Pandas-Example.xlsx')
+# df = pd.read_excel('Pandas-Example.xlsx')
+df = pd.read_excel('Bible.xlsx')
+# print(df)
 
-print(df)
+print("First Name and Last Name From the bible ")
+print(df[['Resident First Name','Resident Last Name']])
+
+for index,row in df.iterrows():
+    print(index,row['Resident First Name'],row['Resident Last Name'])
 
 # print("The list of row indicies")
 # print(df.index)
